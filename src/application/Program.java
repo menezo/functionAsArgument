ecfb699bf17acc67e892785feb27133e56a93c10
Joao Menezes <joaovitormenezesm12@gmail.com>
2022-10-13 14:53:08 -0300

    First implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,29 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import model.entities.Product;
+import model.services.ProductService;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.0));
+		list.add(new Product("Mouse", 50.0));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		ProductService ps = new ProductService();
+		
+		double sum = ps.filteredSum(list);
+		
+		System.out.println("Sum = " + String.format("%.2f", sum));
 
 	}
 
